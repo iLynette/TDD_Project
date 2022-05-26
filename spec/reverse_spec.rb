1094fc51dd6bbe40ec41d4c3e978cce0ff23458f
iLynette <lynetteacholah@icloud.com>
2022-05-26 17:39:01 +0300

    Add test for reverse method

---
+++ spec/reverse_spec.rb
@@ -0,0 +1,13 @@
+describe Solver do
+    before :each do
+      @solver = Solver.new
+    end
+  
+    it 'when given string is empty' do
+      expect(@solver.reverse('')).to eql ''
+    end
+  
+    it 'should return "olleH" when the given word is "Hello"' do
+      expect(@solver.reverse("Hello")).to eql "olleH"
+    end
+  end
