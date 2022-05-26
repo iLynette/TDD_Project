6c815c6fecf00762e941b55c419b135fa7fccb48
iLynette <lynetteacholah@icloud.com>
2022-05-26 16:46:43 +0300

    Test when num is divisible by 5

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -8,4 +8,7 @@ describe Solver do
     it 'When N is divisible by 3, return "fizz"' do
       expect(@solver.fizzbuzz(3)).to eql 'fizz'
     end
+    it 'When N is divisible by 5, return "buzz"' do
+      expect(@solver.fizzbuzz(5)).to eql 'buzz'
+    end
   end
