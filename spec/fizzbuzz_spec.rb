7e0cb5a618068db242e29289854992b5eb7b1e77
iLynette <lynetteacholah@icloud.com>
2022-05-26 16:47:37 +0300

    Test any other case, return num as a string

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -14,5 +14,7 @@ describe Solver do
     it 'When N is divisible by 3 and 5, return "fizzbuzz"' do
       expect(@solver.fizzbuzz(15)).to eql 'fizzbuzz'
     end
-    
+    it 'Any other case, return N as a string' do
+      expect(@solver.fizzbuzz(7)).to eql '7'
+    end
   end
